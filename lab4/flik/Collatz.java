4d1f99f183080f0549f5ab4350717913381d980b
Cheng7x <Cheng7x@users.noreply.github.com>
2026-04-28 21:38:26 +0800

    finish Collatz

---
+++ lab4/flik/Collatz.java
@@ -0,0 +1,17 @@
+package flik;
+
+public class Collatz {
+    public static int nextNumber(int n) {
+        return n % 2 == 0 ? n / 2 : 3 * n + 1;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.print(n + " ");
+        while (n != 1) {
+            n = nextNumber(n);
+            System.out.print(n + " ");
+        }
+        System.out.println();
+    }
+}
